2c5f9cc701444a61e1037c4514d8aa19afcf48c1
fabrizzioRios <warmachine860@gmail.com>
2025-06-05 12:54:09 -0600

    [ADD]-0-First_commit_Angular_Project

---
+++ src/Topics/03-functions.ts
@@ -0,0 +1,27 @@
+
+function addNumbers (a: number, b: number): number {
+    return a + b;
+}
+
+
+const addNumbersArrow = (a: number, b: number): string => {
+    return `${a + b}`
+}
+
+
+function multiply (firstNumber: number, secondNumber?: number, base: number = 2){
+    return firstNumber * base;
+}
+
+
+const result1: number = addNumbers(1, 2);
+const result2: string = addNumbersArrow(1, 2);
+const result3: number = multiply(5);
+
+console.log({
+    result1,
+    result2,
+    result3
+})
+
+export {};
---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import './style.css'
+import './Topics/01-basic-types.ts'
+import './Topics/02-object-interface.ts'
+import './Topics/03-functions.ts'
+
+const app = document.querySelector<HTMLDivElement>('#app')!;
+app.innerHTML = "Hola mundo!";
